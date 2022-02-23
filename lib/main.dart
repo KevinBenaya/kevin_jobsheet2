b870785f7f938baf66563d5ad9c48f7e13e785ee
KevinBenaya <darkfaust063@gmail.com>
2022-02-23 18:09:50 +0700

    kevin_jobsheet2

--- lib/main.dart
+++ lib/main.dart
@@ -141,8 +141,24 @@ class _MyHomePageState extends State<MyHomePage> {
                       width: 50,
                       height: 20,
                     ),
-                    Text(
-                        'Pique Bilang Wasit Untungkan Madrid, Koeman Tepuk Jidat'),
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.black,
+                                  width: 1,
+                                ),
+                              ),
+                              child: Text(
+                                  'Pique Bilang Wasit Untungkan Madrid, Koeman Tepuk Jidat'),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ],
                 ),
                 SizedBox(height: 30),
